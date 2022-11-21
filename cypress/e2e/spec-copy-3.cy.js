624456613510b0d9724632b08db3988c57a1bd22
Hugo BEHAR-THINIERES <hugo.behar.thinieres@gmail.com>
2022-11-21 23:48:25 +0100

    J'ai mis un test ////

--- cypress/e2e/spec-copy-3.cy.js
+++ cypress/e2e/spec-copy-3.cy.js
@@ -4,6 +4,7 @@ describe('duckduckgo', () => {
 cy.visit('https://duckduckgo.com/')
 cy.get('#search_form_input_homepage').type('grafikart.fr')
 cy.get('#search_button_homepage').click({force: true])
+//////TEST//////
   })
 })
 
